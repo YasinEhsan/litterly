2f35fae992c766c63cf1ba3a941f6f3b296f64d7
Joy Paul <41096232+cosmicRover@users.noreply.github.com>
2019-04-11 18:01:33 -0400

    Update AppDelegate.swift

--- litterly/AppDelegate.swift
+++ litterly/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        GMSServices.provideAPIKey("AIzaSyD-SK3TAnQ6-FkP8UbZQ-iXG_q1uCDiMiM")
+        GMSServices.provideAPIKey("api_key_here")
         
         return true
     }
