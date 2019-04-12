fee9887119235395031d51a4257c7b1606ee6d3c
joypaul <jpaul3250@gmail.com>
2019-04-12 19:24:27 -0400

    Added onBoarding and sign-in page

--- litterly/IntroViewController.swift
+++ litterly/IntroViewController.swift
@@ -7,24 +7,34 @@
 //
 
 import UIKit
+import OnboardKit
 
 class IntroViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        onBoardInstances()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //this func stores all of our onboarding data
+    func onBoardInstances() {
+        
+        let pageOne = OnboardPage(title: "Hi there! Welcome to Litterly",
+                                  imageName: "trash",
+                                  description: "a journey to save the city from trash")
+        let pageTwo = OnboardPage(title: "Save the City",
+                                  imageName: "building",
+                                  description: "Meeting others in your city and collect #trashtags to help our environment")
+        let pageThree = OnboardPage(title: "Be a hero!",
+                                  imageName: "statue",
+                                  description: "Earn points and redeem for prizes")
+       
+        //create a onboardingViewController programatically and pass the pages as an array
+        let onboardingViewController = OnboardViewController(pageItems: [pageOne, pageTwo, pageThree])
+        
+        //present the view controller
+        onboardingViewController.presentFrom(self, animated: true)
     }
-    */
 
 }
